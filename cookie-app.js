7834dfc915eba894cc2cfe935d6fee1d00a974c7
ungdeo <1552035329@qq.com>
2023-06-10 16:04:48 +0900

    first cookie

--- cookie-app.js
+++ cookie-app.js
@@ -2,10 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const fs = require('fs');
+const cookieParser = require('cookie-parser');
+
+app.use( cookieParser());//
 
 const PORT = 3000;
 
 app.get('/cookies',(req,res)=>{
+
+    res.setHeader(200,'brand=kim')
     res.sendFile(path.join(__dirname,'views','cookies.html'));
 })
 
