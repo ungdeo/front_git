50f7950c09896c8e53a2cb4f5a414c6b2ad02b01
ungdeo <1552035329@qq.com>
2023-06-10 16:37:11 +0900

    first cookies

--- cookie-app.js
+++ cookie-app.js
@@ -10,8 +10,29 @@ const PORT = 3000;
 
 app.get('/cookies',(req,res)=>{
 
-    res.setHeader(200,'brand=kim')
+    //res.setHeader(200,'brand=kim')
+    //res.cookie('brand','jemicon');
+    const today = new Date();
+    today.setDate(today.getDate() + 10);
+    res.cookie('brand',{
+        name: 'jemicon',
+        id : 'jemicon',
+        Expires: today.toUTCString(),
+        HttpOnly: true,
+        Secure : true
+    })
     res.sendFile(path.join(__dirname,'views','cookies.html'));
+})
+
+app.get('/delCookies',(req,res)=>{
+    res.clearCookie('brand');
+    res.redirect('/showCookies');
+    // front : location.href = '/showCookies'
+})
+
+app.get('/showCookies',(req,res)=>{
+    console.log(req.cookies);
+    res.send(req.cookies);
 })
 
 app.listen(PORT,()=>{
